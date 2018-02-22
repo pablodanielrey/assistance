7d9991d4bd6fcbedc56b357d0ca7f51d228e27dc
Pablo Daniel Rey <pablodanielrey@gmail.com>
2018-02-22 13:57:38 -0300

    agregamos el componente de reporte

--- src/assistance/src/app/app.module.ts
+++ src/assistance/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ReporteComponent } from './reporte/reporte.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReporteComponent
   ],
   imports: [
     BrowserModule
