74ff700d9aba34a48a8128f6ac29151a40326e8f
Pablo Daniel Rey <pablodanielrey@gmail.com>
2018-02-24 19:09:35 -0300

    agregue autentificacion al sistema

--- src/assistance/src/app/app.module.ts
+++ src/assistance/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { OAuthModule } from 'angular-oauth2-oidc';
 
 import { AppComponent } from './app.component';
 import { ReporteComponent } from './reporte/reporte.component';
@@ -14,7 +15,8 @@ import { AppRoutingModule } from './/app-routing.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    OAuthModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
