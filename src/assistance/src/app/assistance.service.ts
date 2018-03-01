6a624e631b6b45240968abc2277ee57f1acaab50
Emanuel Pais <emanuel@econo.unlp.edu.ar>
2018-03-01 10:17:47 -0300

    Solucione el error del service: A tuple type element list cannot be empty

--- src/assistance/src/app/assistance.service.ts
+++ src/assistance/src/app/assistance.service.ts
@@ -22,12 +22,12 @@ export class AssistanceService {
 
   obtenerRelojes(): Observable<Reloj[]> {
     let apiUrl = `${ASSISTANCE_API_URL}/relojes`;
-    return this.http.get<[]>(apiUrl).map(datos => datos.map(d => new Reloj(d)));
+    return this.http.get<[Reloj[]]>(apiUrl).map(datos => datos.map(d => new Reloj(d)));
   }
 
   obtenerReloj(rid:string): Observable<Reloj> {
     let apiUrl = `${ASSISTANCE_API_URL}/relojes/${rid}`;
-    return this.http.get<[]>(apiUrl).map(datos => new Reloj(datos));
+    return this.http.get<[Reloj]>(apiUrl).map(datos => new Reloj(datos));
   }
 
 
