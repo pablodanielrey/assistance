07c38ed9b453e6367a99493768b6d8e3413fabba
Pablo Daniel Rey <pablodanielrey@gmail.com>
2018-03-28 15:38:36 -0300

    dejamos para poder pasarlo a producción con correcciones simples

--- src/assistance/src/app/justificacion/justificacion-personal/justificacion-personal.component.ts
+++ src/assistance/src/app/justificacion/justificacion-personal/justificacion-personal.component.ts
@@ -84,6 +84,12 @@ export class JustificacionPersonalComponent implements OnInit {
     this.location.back();
   }
 
+  obtenerDias(): number {
+    let dt1 = new Date(this.fechaInicio);
+    let dt2 = new Date(this.fechaFin);
+    return Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate()) ) /(1000 * 60 * 60 * 24));
+  }
+
   justificar() {
     let fj=  new FechaJustificada({});
     fj.usuario_id = this.usuario_id;
