12f527aa3773ed8ddf011f8f040f0b566744360d
Walter Blanco <walter.blanco@econo.unlp.edu.ar>
2018-04-04 11:12:43 -0300

    Estile la pantalla de justificacion general

--- src/assistance/src/app/justificacion/justificacion-general/justificacion-general.component.ts
+++ src/assistance/src/app/justificacion/justificacion-general/justificacion-general.component.ts
@@ -68,6 +68,12 @@ export class JustificacionGeneralComponent implements OnInit {
     this.location.back();
   }
 
+  obtenerDias(): number {
+    let dt1 = new Date(this.fechaInicio);
+    let dt2 = new Date(this.fechaFin);
+    return Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate()) ) /(1000 * 60 * 60 * 24));
+  }
+
   justificar() {
     let fj=  new FechaJustificada({});
     fj.justificacion = this.justificacion;
