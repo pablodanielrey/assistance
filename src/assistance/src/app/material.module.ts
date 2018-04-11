ce0020d743eab2d0fae0039c832d341416db5019
Walter Blanco <walter.blanco@econo.unlp.edu.ar>
2018-04-11 14:53:50 -0300

    Termine de estilar la pantalla de reportes generales

--- src/assistance/src/app/material.module.ts
+++ src/assistance/src/app/material.module.ts
@@ -21,6 +21,7 @@ import { MatButtonModule,
          MatSortModule,
          MatNativeDateModule,
          MatTooltipModule,
+         MatExpansionModule,
          MatSnackBarModule
        } from '@angular/material';
 
@@ -50,6 +51,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatTabsModule,
     MatSortModule,
     MatSnackBarModule,
+    MatExpansionModule,
     BrowserAnimationsModule
   ],
   exports: [
@@ -75,6 +77,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatTabsModule,
     MatSortModule,
     MatSnackBarModule,
+    MatExpansionModule,
     BrowserAnimationsModule
   ],
 })
