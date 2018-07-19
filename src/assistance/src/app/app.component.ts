081744e06eeef94f5397edfe304c0f1aa1cd9805
Pablo Daniel Rey <pablodanielrey@gmail.com>
2018-07-19 19:17:17 -0300

    parametrice los datos del oidp

--- src/assistance/src/app/app.component.ts
+++ src/assistance/src/app/app.component.ts
@@ -4,13 +4,15 @@ import { Router } from '@angular/router';
 // configuro la autentificacion
 import { AuthConfig } from 'angular-oauth2-oidc';
 
+import { environment } from '../environments/environment';
+
 export const authConfig: AuthConfig = {
-  issuer: 'https://oidp.econo.unlp.edu.ar',
+  issuer: environment.oidp_issuer,
   redirectUri: window.location.origin,
   // tokenEndpoint: 'https://oidp.econo.unlp.edu.ar/oauth2/auth',
-  userinfoEndpoint: 'https://oidp.econo.unlp.edu.ar/userinfo',
-  loginUrl: 'https://oidp.econo.unlp.edu.ar/oauth2/auth',
-  logoutUrl: 'https://consent.econo.unlp.edu.ar/logout',
+  userinfoEndpoint: environment.userinfoEndpoint,
+  loginUrl: environment.loginUrl,
+  logoutUrl: environment.logoutUrl,
   oidc: true,
   requireHttps: false,
   clientId: 'assistance-ui',
@@ -18,6 +20,7 @@ export const authConfig: AuthConfig = {
   scope: 'openid profile email',
   showDebugInformation: true
 }
+
 import { OAuthService } from 'angular-oauth2-oidc';
 import { NullValidationHandler, JwksValidationHandler } from 'angular-oauth2-oidc';
 
--- src/assistance/src/environments/environment.ts
+++ src/assistance/src/environments/environment.ts
@@ -5,5 +5,10 @@
 
 export const environment = {
   production: false,
-  assistanceApiUrl: 'https://api.econo.unlp.edu.ar/assistance/api/v1.0'
+  assistanceApiUrl: 'https://api.asistencia.presi.unlp.edu.ar/assistance/api/v1.0',
+  oidp_issuer: 'https://oidp.asistencia.presi.unlp.edu.ar',
+  userinfoEndpoint: 'https://oidp.asistencia.presi.unlp.edu.ar/userinfo',
+  loginUrl: 'https://oidp.asistencia.presi.unlp.edu.ar/oauth2/auth',
+  logoutUrl: 'https://consent.asistencia.presi.unlp.edu.ar/logout'
+  // tokenEndpoint: 'https://oidp.econo.unlp.edu.ar/oauth2/auth'
 };
