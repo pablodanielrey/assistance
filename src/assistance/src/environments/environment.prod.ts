bb6510659a2466537815aa231a0c3960efdbddf2
Pablo Daniel Rey <pablodanielrey@gmail.com>
2018-08-16 07:59:48 -0300

    para que se pueda hacer deploy en econo

--- src/assistance/src/environments/environment.prod.ts
+++ src/assistance/src/environments/environment.prod.ts
@@ -1,17 +1,12 @@
 export const environment = {
   production: true,
-<<<<<<< HEAD
-  assistanceApiUrl: 'https://api.asistencia.presi.unlp.edu.ar/assistance/api/v1.0'
-  // assistanceApiUrl: 'https://api.econo.unlp.edu.ar/assistance/api/v1.0'
-=======
-  assistanceApiUrl: 'https://api.asistencia.presi.unlp.edu.ar/assistance/api/v1.0',
-  oidp_issuer: 'https://oidp.asistencia.presi.unlp.edu.ar',
-  userinfoEndpoint: 'https://oidp.asistencia.presi.unlp.edu.ar/userinfo',
-  loginUrl: 'https://oidp.asistencia.presi.unlp.edu.ar/oauth2/auth',
-  logoutUrl: 'https://consent.asistencia.presi.unlp.edu.ar/logout'
+  assistanceApiUrl: 'https://api.econo.unlp.edu.ar/assistance/api/v1.0',
+  oidp_issuer: 'https://oidp.econo.unlp.edu.ar',
+  userinfoEndpoint: 'https://oidp.econo.unlp.edu.ar/userinfo',
+  loginUrl: 'https://oidp.econo.unlp.edu.ar/oauth2/auth',
+  logoutUrl: 'https://consent.econo.unlp.edu.ar/logout'
   // tokenEndpoint: 'https://oidp.econo.unlp.edu.ar/oauth2/auth'
 
   
 
->>>>>>> origin/dev
 };
