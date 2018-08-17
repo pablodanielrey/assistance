528bdd25dff69bfef0b9e9020cce6fd78a82a5b3
Pablo Daniel Rey <pablodanielrey@gmail.com>
2018-08-17 15:42:25 -0300

    agregamos la selección de fecha y el mostrar el reporte personal.

--- src/assistance/src/app/miperfil/miperfil.component.ts
+++ src/assistance/src/app/miperfil/miperfil.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { OAuthService } from 'angular-oauth2-oidc';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { Usuario } from '../entities/usuario';
+
 @Component({
   selector: 'app-miperfil',
   templateUrl: './miperfil.component.html',
   styleUrls: ['./miperfil.component.css']
 })
 export class MiperfilComponent implements OnInit {
+
+  fecha: Date = new Date();
+  usuario: Usuario;
+  info: any;
 
   oficinas=[
     {
@@ -26,9 +35,35 @@ export class MiperfilComponent implements OnInit {
   ]
 
 
-  constructor() { }
+  constructor(private router: Router, private oauthService: OAuthService) { }
 
   ngOnInit() {
+    this.info = this.oauthService.getIdentityClaims();
+    // TODO: consultar la entidad usuario correcta.
+    this.usuario = new Usuario(
+      {
+        id: this.info.sub, 
+        nombre: this.info.name
+      }
+    );
+  }
+
+  obtener_fecha_final() {
+    return this.fecha.toISOString();
+  }
+
+  obtener_fecha_inicial() {
+    let data = (new Date(this.fecha.getTime() - (7 * 24 * 60 * 60 * 1000)).toISOString());
+    return data;
+  }
+
+  ver_reporte() {
+    this.router.navigate(['/sistema/reportes/personal/' + this.usuario.id, 
+      {
+        fecha_inicial:this.obtener_fecha_inicial(), 
+        fecha_final:this.obtener_fecha_final()
+      }
+    ]);
   }
 
 }
