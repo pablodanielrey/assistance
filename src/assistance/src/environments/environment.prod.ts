6c16b82da927de14265d6c716985ebda87216215
Emanuel Pais <emanuel84@gmail.com>
2018-08-23 12:34:58 -0300

    Se parametrizo assistance: dockerfile y environment. Falta imagenes y textos

--- src/assistance/src/environments/environment.prod.ts
+++ src/assistance/src/environments/environment.prod.ts
@@ -1,12 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
 export const environment = {
-  production: true,
-  assistanceApiUrl: 'https://api.asistencia.presi.unlp.edu.ar/assistance/api/v1.0',
-  oidp_issuer: 'https://oidp.asistencia.presi.unlp.edu.ar',
-  userinfoEndpoint: 'https://oidp.asistencia.presi.unlp.edu.ar/userinfo',
-  loginUrl: 'https://oidp.asistencia.presi.unlp.edu.ar/oauth2/auth',
-  logoutUrl: 'https://consent.asistencia.presi.unlp.edu.ar/logout'
+  production: false,
+  assistanceApiUrl: 'https://api.econo.unlp.edu.ar/assistance/api/v1.0',
+  oidp_issuer: 'https://oidp.econo.unlp.edu.ar',
+  userinfoEndpoint: 'https://oidp.econo.unlp.edu.ar/userinfo',
+  loginUrl: 'https://oidp.econo.unlp.edu.ar/oauth2/auth',
+  logoutUrl: 'https://consent.econo.unlp.edu.ar/logout'
   // tokenEndpoint: 'https://oidp.econo.unlp.edu.ar/oauth2/auth'
 
-  
 
 };
