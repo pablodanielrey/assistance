57501c6a52feb9ea70ec84e03753e6f522027227
Emanuel Pais <emanuel84@gmail.com>
2018-09-06 11:00:25 -0300

    agregar en el menu de asistencia el item reportes al usuario comun

--- src/assistance/src/app/menu/menu.component.ts
+++ src/assistance/src/app/menu/menu.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Location } from '@angular/common';
 import { Router } from '@angular/router';
 
 import { MatSidenav } from '@angular/material/sidenav';
 import { OAuthService } from 'angular-oauth2-oidc';
+import { Usuario } from '../entities/usuario';
 
 import { AssistanceService } from '../assistance.service';
 import { environment } from '../../environments/environment';
@@ -20,10 +22,13 @@ export class MenuComponent implements OnInit {
 
   modulos: string[] = [];
   subscriptions: any[] = [];
+  usuarioId: string;
 
-  constructor(private oauthService: OAuthService, private router: Router, private service: AssistanceService) { }
+  constructor(private oauthService: OAuthService, private router: Router, private service: AssistanceService, private location: Location) { }
 
   ngOnInit() {
+    let info: any = this.oauthService.getIdentityClaims();
+    this.usuarioId = info.sub;
     this.subscriptions.push(this.service.obtenerAccesoModulos().subscribe(modulos => {
       this.modulos = modulos;
       console.log(this.modulos);
@@ -42,6 +47,27 @@ export class MenuComponent implements OnInit {
   inicial():void {
     this.onInternalItem();
     this.router.navigate(['/sistema', {outlets: {'pantalla': ['inicial']}}]);
+  }
+
+  obtener_fecha_final(fecha: Date) {
+    return fecha.toISOString();
+  }
+
+  obtener_fecha_inicial(fecha: Date) {
+    let data = (new Date(fecha.getTime() - (7 * 24 * 60 * 60 * 1000)).toISOString());
+    return data;
+  }
+
+  ver_reporte_personal(): void {
+    let path = this.location.path().split(';');
+
+    this.router.navigate(['/sistema/reportes/personal/' + this.usuarioId,
+      {
+        fecha_inicial:this.obtener_fecha_inicial(new Date()),
+        fecha_final:this.obtener_fecha_final(new Date()),
+        back: path[0]
+      }
+    ]);
   }
 
   onOpenedChange(event: boolean):void {
