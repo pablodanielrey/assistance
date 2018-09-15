f8233d0f7cb3445c2f4d402bcce604773ddd7ed1
Pablo Daniel Rey <pablodanielrey@gmail.com>
2018-09-15 16:45:22 -0300

    tenian api_test

--- src/assistance/src/environments/environment.presi.ts
+++ src/assistance/src/environments/environment.presi.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  assistanceApiUrl: 'https://api.asistencia.presi.unlp.edu.ar/assistance/api_test/v1.0',
+  assistanceApiUrl: 'https://api.asistencia.presi.unlp.edu.ar/assistance/api/v1.0',
   oidp_issuer: 'https://oidp.asistencia.presi.unlp.edu.ar/',
   logoutUrl: 'https://login.asistencia.presi.unlp.edu.ar/logout',
   telegramURL: 'https://telegram.me/AsistenciaPresiBot?start=',
