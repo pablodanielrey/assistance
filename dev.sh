8d842dcbbaef98ef98fda305efc35a61259212d4
Pablo Daniel Rey <pablodanielrey@gmail.com>
2018-09-19 17:36:13 -0300

    agregue la redireccion del puerto

--- dev.sh
+++ dev.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -ti --name assistance-ui -v $(pwd)/src:/src -p 4202:4200 assistance-ui /bin/sh
+docker run --rm -ti --name assistance-ui -v $(pwd)/src:/src -p 10305:4200 assistance-ui /bin/sh
