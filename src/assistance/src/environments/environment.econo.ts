b65add973d1571a08c7e4318bf9564944192d68c
Pablo Daniel Rey <pablodanielrey@gmail.com>
2018-10-10 19:42:18 -0300

    faltaba activar el service worker

--- src/assistance/src/environments/environment.econo.ts
+++ src/assistance/src/environments/environment.econo.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   assistanceApiUrl: 'https://api.econo.unlp.edu.ar/assistance/api/v1.0',
   oidp_issuer: 'https://oidc.econo.unlp.edu.ar/',
   logoutUrl:'https://login.econo.unlp.edu.ar/logout/{{id_token}}/{{client_id}}',
