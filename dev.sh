85267e4dae13dc1297c2fdc97ca035a8091bfae8
Pablo Daniel Rey <pablodanielrey@gmail.com>
2018-10-13 15:32:08 -0300

    actualice a la nueva versión del esquema de desarrollo. basado en el repo desarrollo-ui y las instrucciones del redmine dentro de la wiki

--- dev.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "corriendo en el puerto 10305"
-docker run --rm -ti --name assistance-ui -v $(pwd)/src:/src -p 10305:4200 assistance-ui /bin/sh
--- src/servir-vs.sh
+++
@@ -1,2 +0,0 @@
-cd /src/assistance/src/
-ng serve --host 0.0.0.0 --configuration=vs --disable-host-check
